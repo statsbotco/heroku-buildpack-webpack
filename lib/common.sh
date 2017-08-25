254c7025968ef4828e15c99b37518bd2f63cef95
Pavel Tiunov <pavel.tiunov@gmail.com>
2017-08-26 01:02:11 +0300

    Webpack cache

--- lib/common.sh
+++ lib/common.sh
@@ -15,3 +15,37 @@ export_env_dir() {
     done
   fi
 }
+
+restore_cache_directories() {
+  local build_dir=${1:-}
+  local cache_dir=${2:-}
+
+  for cachepath in ${@:3}; do
+    if [ -e "$build_dir/$cachepath" ]; then
+      echo "- $cachepath (exists - skipping)"
+    else
+      if [ -e "$cache_dir/node/$cachepath" ]; then
+        echo "- $cachepath"
+        mkdir -p $(dirname "$build_dir/$cachepath")
+        mv "$cache_dir/node/$cachepath" "$build_dir/$cachepath"
+      else
+        echo "- $cachepath (not cached - skipping)"
+      fi
+    fi
+  done
+}
+
+save_cache_directories() {
+  local build_dir=${1:-}
+  local cache_dir=${2:-}
+
+  for cachepath in ${@:3}; do
+    if [ -e "$build_dir/$cachepath" ]; then
+      echo "- $cachepath"
+      mkdir -p "$cache_dir/node/$cachepath"
+      cp -a "$build_dir/$cachepath" $(dirname "$cache_dir/node/$cachepath")
+    else
+      echo "- $cachepath (nothing to cache)"
+    fi
+  done
+}
